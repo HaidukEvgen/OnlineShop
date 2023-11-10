84f293797492d7370bce8ea6e6598c89ecd72118
kanura56@gmail.com <kanura56@gmail.com>
2023-11-10 11:37:06 +0300

    Add CatalogAPI

---
+++ OnlineShop.Services.CatalogAPI/MiddlewareHandlers/GlobalErrorHandler.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using OnlineShop.Services.CatalogAPI.Exceptions;
+using OnlineShop.Services.CatalogAPI.Models.Dto;
+using System.Net;
+using System.Text.Json;
+namespace OnlineShop.Services.CatalogAPI.MiddlewareHandlers
+{
+    public class GlobalErrorHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                response.StatusCode = error switch
+                {
+                    ValidationException => (int)HttpStatusCode.BadRequest,
+                    ProductNotFoundException => (int)HttpStatusCode.NotFound,
+                    _ => (int)HttpStatusCode.InternalServerError,
+                };
+
+                var result = JsonSerializer.Serialize(new ResponseDto() { IsSuccess = false, Message = error.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
---
+++ OnlineShop.Services.CatalogAPI/Models/Data/CatalogDbOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.Services.CatalogAPI.Models.Data
+{
+    public class CatalogDbOptions
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
