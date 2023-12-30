bf0af70b04703626df817dd8591ca071f94f21f8
kanura56@gmail.com <kanura56@gmail.com>
2023-12-30 10:13:11 +0300

    implement email service

---
+++ OnlineShop.Services.Order.BusinessLayer/Services/Implementations/EmailService.cs
@@ -0,0 +1,27 @@
+﻿using OnlineShop.Services.Order.BusinessLayer.Services.Interfaces;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+namespace OnlineShop.Services.Order.BusinessLayer.Services.Implementations
+{
+    public class EmailService : IEmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string messageBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(EmailSetup.MailBoxName, EmailSetup.MailBoxAddress));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = subject;
+            message.Body = new TextPart("plain") { Text = messageBody };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(EmailSetup.EmailHost, EmailSetup.EmailHostPort, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(EmailSetup.MailBoxAddress, EmailSetup.MailBoxPassword);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
---
+++ OnlineShop.Services.Order.BusinessLayer/Services/Implementations/EmailSetup.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.Services.Order.BusinessLayer.Services.Implementations
+{
+    public static class EmailSetup
+    {
+        public const string MailBoxName = "Online shop Order service";
+        public const string MailBoxAddress = "kanura56@gmail.com";
+        public const string MailBoxPassword = "qenl revv lyft lhud";
+        public const string EmailHost = "smtp.gmail.com";
+        public const int EmailHostPort= 587;
+    }
+}
